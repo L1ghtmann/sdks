b6696a2083c2dd7baebbaf43abf5523b45c476fe
Lightmann <lightmanns11@gmail.com>
2023-12-30 19:41:22 -0500

    Finally?

--- create_patched_sdk.sh
+++ create_patched_sdk.sh
@@ -116,13 +116,7 @@ if [[ $sdk_platform == iOS ]]; then
 	xcode_plat_sdks_path="$xcode_developer_path/Platforms/iPhoneOS.platform/Developer/SDKs"
 	xcode_default_sdk_path="$xcode_plat_sdks_path/iPhoneOS.sdk"
 else
-	echo 1
-	ls -A "$xcode_developer_path/Platforms/AppleTVOS.platform/"
-	echo 2
-	ls -A "$xcode_developer_path/Platforms/AppleTVOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes"
-	echo 3
-	ls -A "$xcode_developer_path/Platforms/AppleTVOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/tvOS.simruntime/"
-
+	find "$xcode_developer_path/Platforms/AppleTVOS.platform" -name "RuntimeRoot" -print
 	xcode_sim_runtime_path="$xcode_developer_path/Platforms/AppleTVOS.platform/Library/Developer/CoreSimulator/Profiles/Runtimes/tvOS.simruntime/Contents/Resources/RuntimeRoot"
 	if ! [[ -d $xcode_sim_runtime_path ]]; then
 		xcode_sim_runtime_path="$xcode_developer_path/Platforms/AppleTVOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/tvOS.simruntime/Contents/Resources/RuntimeRoot"
